e01d074c1f6dff28f5dfa5b7d18efd872686374a
Marko Ludlow <ludlow.marko@gmail.com>
2017-05-12 10:42:21 -0700

    add basic add function to website.

--- app.rb
+++ app.rb
@@ -5,3 +5,39 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get "/" do
   erb :index
 end
+
+get("/brands") do
+  @brands = Brand.all()
+  erb(:brands)
+end
+
+get("/stores") do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+post("/brands") do
+  name = params.fetch("brand_name")
+  Brand.create({:name => name})
+  redirect("/brands")
+end
+
+post("/stores") do
+  name = params.fetch("store_name")
+  Store.create({:name => name})
+  redirect("/stores")
+end
+
+get("/brands/:id") do
+  @brand = Brand.find(params.fetch("id").to_i())
+  @brand_stores = @brand.stores
+  @unassigned_stores = Store.includes(:brands).where(:stores_brands => {:brand_id => nil})
+  erb(:brand)
+end
+
+get("/stores/:id") do
+  @store = Store.find(params.fetch("id").to_i())
+  @store_brands = @store.brands
+  @unassigned_brands = Brand.includes(:stores).where(:stores_brands => {:store_id => nil})
+  erb(:store)
+end
