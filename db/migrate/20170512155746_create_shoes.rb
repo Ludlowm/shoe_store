a99219c29c12feac9ca400cd734e095a471d18e8
Marko Ludlow <ludlow.marko@gmail.com>
2017-05-12 09:17:47 -0700

    add backend database creation

---
+++ db/migrate/20170512155746_create_shoes.rb
@@ -0,0 +1,10 @@
+class CreateShoes < ActiveRecord::Migration[5.1]
+  def change
+    create_table(:shoes) do |t|
+      t.column(:name, :string)
+      t.column(:description, :string)
+
+      t.timestamps()
+    end
+  end
+end
